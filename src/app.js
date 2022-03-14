841d9ff6bb2e94edf5011767acb2c16b3f62f657
German Cosio <german.cosio1@ibm.com>
2022-03-14 08:10:26 -0600

    enable socket.io

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 const port = process.env.port || 3000;
 app.use(express.static('./public'));
 
-app.listen(3000, () => {
+io.on('connection', () => {
+  console.log('New WebSocket connection');
+});
+
+server.listen(3000, () => {   
   console.log(`Server is up on port ${port}`);
 });
