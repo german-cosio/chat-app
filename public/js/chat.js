a56b688697e5f3dd8438436a2f63eeac97133f66
German Cosio <german.cosio1@ibm.com>
2022-04-06 08:45:14 -0500

    add more styles and functionality

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,75 @@
-io()
+const socket = io();
+
+//Elements
+const chatForm = document.querySelector('#chat-form');
+const sendLocationButton = document.querySelector('#send-location');
+const messageInput = document.querySelector('#message');
+const sendButton = document.querySelector('#send-button');
+const messages = document.querySelector('#messages');
+
+//Templates
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationTemplate = document.querySelector('#location-template').innerHTML;
+
+const { username, room }= Qs.parse(location.search, {
+     ignoreQueryPrefix: true
+});
+
+// Listeners
+socket.on('message', (message) => { 
+     console.log(message);
+     const html = Mustache.render(messageTemplate, {
+           message: message.text,
+           createdAt: moment(message.createdAt).format('h:mm a'),
+     });
+     messages.insertAdjacentHTML('beforeend', html);
+     });
+
+socket.on('locationMessage', (location) => { 
+     const html = Mustache.render(locationTemplate, {
+               url: location.url,
+               createdAt: moment(location.createdAt).format('h:mm a'),
+     });
+    messages.insertAdjacentHTML('beforeend', html);
+     console.log(location);
+     });
+
+// Emitters
+chatForm.addEventListener('submit', (e) => {
+     e.preventDefault();
+     sendButton.setAttribute('disabled', 'disabled');
+     const message = e.target.elements.message.value;
+     socket.emit('sendMessage', message, (serverMessage) => {
+          console.log(serverMessage);
+     });
+     sendButton.removeAttribute('disabled');
+     messageInput.value = '';
+     messageInput.focus();
+});
+
+sendLocationButton.addEventListener('click', () => {
+     if (!navigator.geolocation) {
+          return alert('Geolocation is not supported by your browser.');
+     }
+     sendLocationButton.setAttribute('disabled', 'disabled');
+     sendLocationButton.textContent = 'Sending location...';
+     navigator.geolocation.getCurrentPosition((position) => {
+          sendLocationButton.removeAttribute('disabled');
+          sendLocationButton.textContent = 'Send location';
+          socket.emit('sendLocation', {
+               latitude: position.coords.latitude,
+               longitude: position.coords.longitude
+          },
+          (serverMessage) => {
+               console.log(serverMessage);
+          }
+          );
+     }, () => {
+          sendLocationButton.removeAttribute('disabled');
+          sendLocationButton.textContent = 'Send location';
+          alert('Unable to fetch location.');
+     });
+     }
+     );
+
+     socket.emit('join', { username, room } );
--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
+const Filter = require('bad-words');
+const { generateMessage, generateLocationMessage } = require('./utils/messages');
 
 const app = express();
 const server = http.createServer(app);
@@ -9,9 +11,35 @@ const io = socketIO(server);
 const port = process.env.port || 3000;
 app.use(express.static('./public'));
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
   console.log('New WebSocket connection');
-});
+
+  socket.on('join', ( { username, room } ) => {
+    socket.join(room);
+    socket.emit('message', generateMessage(`Welcome to ${room}`));
+    socket.broadcast.to(room).emit('message', generateMessage(`${username} has joined!`));
+  });
+
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+    if(filter.isProfane(message)) {
+      return callback('Profanity is not allowed!');
+    }
+    io.emit('message', generateMessage(message));
+  });
+
+  socket.on('sendLocation', (location, callback) => {
+    // socket.broadcast.emit('locationMessage', `https://google.com/maps?q=${location.latitude},${location.longitude}`);
+    const url = `https://google.com/maps?q=${location.latitude},${location.longitude}`;
+    io.emit('locationMessage', generateLocationMessage(url));
+    callback('Location shared!');
+  });
+  socket.on('disconnect', () => {
+    io.emit('message', generateMessage('A user has left!'));
+    // io.emit('message', generateMessage(`${username} has left.`))
+    // socket.broadcast.to(room).emit('message', generateMessage(`${username} has left.`));
+  });
+  });
 
 server.listen(3000, () => {   
   console.log(`Server is up on port ${port}`);
